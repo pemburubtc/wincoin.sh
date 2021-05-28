890077e322824bf40e7e9e79e9141783e6db04d9
pemburubtc <36067824+pemburubtc@users.noreply.github.com>
2021-05-29 06:26:09 +0800

    Create Wincoin.sh

---
+++ Wincoin.sh
@@ -0,0 +1,4 @@
+# wincoin
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u WIN:TRyVtFrivtNifjhFhMq23wJsQvxfi47r6X.Gadiz1
